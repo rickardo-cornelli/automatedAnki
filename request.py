edf9a667fd3e5c6d8fb9c75d6ce4f5e5efbecac2
rickardo-cornelli <rickardcornell@gmail.com>
2024-12-22 13:00:00 +0100

    issue #1 - feat: implemented checks for status codes and giving messages

--- request.py
+++ request.py
@@ -8,25 +8,46 @@ from dotenv import load_dotenv
 # Preload supported spaCy models
 SPACY_MODELS = {
     "de": spacy.load("de_core_news_sm"),  # German
+    "sv": spacy.load('sv_core_news_sm'),
+    "fr": spacy.load('fr_core_news_sm')
 }
 
+def isNoun(word_response):
+    return word_response["results"][0]["headword"]["pos"] == "noun"
+
 def get_definition(word, language="de"):
+    print(word)
     load_dotenv(dotenv_path='Anki.env')
     base_word = get_base_form(word,language)
+    print(base_word)
 
     RAPIDAPI_KEY = os.getenv('RAPIDAPI_KEY')
     RAPIDAPI_HOST = os.getenv('RAPIDAPI_HOST')
 
     url = "https://lexicala1.p.rapidapi.com/search-entries"
-    querystring = {"text": base_word, "language": language, "analyzed": "true"}
+    querystring = {"text": base_word, "language": "language", "analyzed": "true"}
     headers = {
         "X-RapidAPI-Key": RAPIDAPI_KEY,
         "X-RapidAPI-Host": RAPIDAPI_HOST
     }
 
-    response = requests.get(url, headers=headers, params=querystring)
-    print(response.text)
+    response = requests.get(url, headers=headers, params=querystring, timeout=10)
+    if(response.status_code == 200):
+        response_data = response.json()
+
+        if response_data["results"]:
+            if(isNoun(response_data)):
+                print(f"{base_word} is a noun")
+            else:
+                print(f"{base_word} is not a noun")
+        else:
+            print(f"No valid response for {base_word} in {language}, verify the spelling of both")
+    elif(response.status_code == 403):
+        print("Status code 403, verify that you have set up your API key correctly")
+    else:
+        print(f"Request failed with status code {response.status_code}")
     return None  # Indicate failure
+
 
 def lemmatize_fallback(word, language):
     # Check if spaCy model for the language is loaded
@@ -44,4 +65,4 @@ def get_base_form(word, language="de"):
     return lemma
 
 # Test words in multiple languages
-get_definition("Männer", "de")
+get_definition("hursm", "sv")
